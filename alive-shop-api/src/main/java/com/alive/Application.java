496d00b582ee2c85085897795c83521020f3f5c5
ymk <1934495742@qq.com>
2019-11-17 17:21:18 +0800

    简单测试启动hello

---
+++ alive-shop-api/src/main/java/com/alive/Application.java
@@ -0,0 +1,1 @@
+package com.alive;import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;/** * Created by mingkunyu on 2019-11-17 */@SpringBootApplicationpublic class Application {    public static void main(String[] args) {        SpringApplication.run(Application.class,args);    }}
