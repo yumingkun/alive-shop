a4839ae65e03b43e70a3884a13fe293a082987b8
ymk <1934495742@qq.com>
2019-11-24 20:05:53 +0800

    编写简单测试方法

--- alive-shop-mapper/src/main/java/com/alive/mapper/StuMapper.java
+++ alive-shop-mapper/src/main/java/com/alive/mapper/StuMapper.java
@@ -1,7 +1,9 @@
 package com.alive.mapper;
 
 import com.alive.Stu;
+import tk.mybatis.mapper.common.Mapper;
+import tk.mybatis.mapper.common.MySqlMapper;
 
 
-public interface StuMapper extends tk.mybatis.mapper.common.Mapper<Stu>, tk.mybatis.mapper.common.MySqlMapper<Stu> {
+public interface StuMapper extends Mapper<Stu>, MySqlMapper<Stu> {
 }
